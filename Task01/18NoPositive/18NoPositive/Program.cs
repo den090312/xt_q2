61a68f86e4d7fd3331a4e3befadcaa87a8405a2a
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-02 00:16:46 +0400

    no message

---
+++ Task01/18NoPositive/18NoPositive/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace _18NoPositive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxValue = 101;
+            int minValue = -1 * maxValue;
+            int[,,] myArray = GetRandomArray(new Random(), maxValue, minValue);
+
+            Console.WriteLine("Трёхмерный массив случайных чисел от "+ (minValue + 1) + " до "+ (maxValue - 1) + ":");
+            Console.WriteLine("");
+            WriteArray(myArray);
+
+            Console.WriteLine("Заменяем положительные элементы на нули:");
+            Console.WriteLine("");
+
+            WriteArray(GetNoPositiveArray(myArray));
+        }
+
+        static void WriteArray(int[,,] myArray)
+        {
+            for (int x = 0; x <= 4; x++)
+                WriteY(x, myArray);
+        }
+
+        static void WriteY(int x, int[,,] myArray)
+        {
+            for (int y = 0; y <= 4; y++)
+                WriteZ(x, y, myArray);
+            Console.WriteLine();
+        }
+
+        static void WriteZ(int x, int y, int[,,] myArray)
+        {
+            for (int z = 0; z <= 4; z++)
+                Console.Write(myArray[x, y, z] + " ");
+            Console.WriteLine();
+        }
+
+        static int[,,] GetNoPositiveArray(int[,,] myArray)
+        {
+            for (int x = 0; x <= 4; x++)
+            {
+                if (myArray[x, 0, 0] > 0)
+                    myArray[x, 0, 0] = 0;
+                SetNoPositiveY(x, myArray);
+            }
+
+            return myArray;
+        }
+
+        static void SetNoPositiveY(int x, int[,,] myArray)
+        {
+            for (int y = 0; y <= 4; y++)
+            {
+                if (myArray[x, y, 0] > 0)
+                    myArray[x, y, 0] = 0;
+                SetNoPositiveZ(x, y, myArray);
+            }
+        }
+
+        static void SetNoPositiveZ(int x, int y, int[,,] myArray)
+        {
+            for (int z = 0; z <= 4; z++)
+            {
+                if (myArray[x, y, z] > 0)
+                    myArray[x, y, z] = 0;
+            }
+        }
+
+        static int[,,] GetRandomArray(Random myRandom, int maxValue, int minValue)
+        {
+            int[,,] myArray = new int[5, 5, 5]
+            {
+                {
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                },
+                {
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                },
+                {
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                },
+                {
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                },
+                {
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                    { myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue), myRandom.Next(minValue, maxValue) },
+                },
+            };
+
+            return myArray;
+        }
+    }
+}
