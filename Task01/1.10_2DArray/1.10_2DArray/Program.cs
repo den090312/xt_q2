acd33c245cb61e17729f3f2e8fb0c1c12c085aaa
Denis Bolotin <31979965+den090312@users.noreply.github.com>
2019-07-02 14:58:31 +0400

    Add files via upload

---
+++ Task01/1.10_2DArray/1.10_2DArray/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._10_2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = GetRandom2DArray();
+
+            Console.WriteLine("Двумерный массив случайных чисел:");
+            Console.WriteLine("");
+
+            Write2DArray(myArray);
+            Console.WriteLine("");
+
+            Console.WriteLine("Сумма элементов массива, стоящих на чётных позициях, равна: "+ GetSumOddPosElements(myArray));
+        }
+
+        static int[,] GetRandom2DArray()
+        {
+            Random myRandom = new Random();
+            int[,] myArray = new int[10, 10];
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    myArray[i, j] = myRandom.Next(0, 9);
+                }
+            }
+
+            return myArray;
+        }
+
+        static void Write2DArray(int[,] myArray)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    Console.Write(myArray[i,j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static int GetSumOddPosElements(int[,] myArray)
+        {
+            int sum = 0;
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = 0; j <= 9; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        sum += myArray[i, j];
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
