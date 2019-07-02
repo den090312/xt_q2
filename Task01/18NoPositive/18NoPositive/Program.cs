998731486618356cc7403a2579c0b2936800ea76
Denis Bolotin <31979965+den090312@users.noreply.github.com>
2019-07-02 11:07:27 +0400

    Update Program.cs

--- Task01/18NoPositive/18NoPositive/Program.cs
+++ Task01/18NoPositive/18NoPositive/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _18NoPositive
 {
@@ -10,29 +10,29 @@ namespace _18NoPositive
             int minValue = -1 * maxValue;
             int[,,] myArray = GetRandomArray(maxValue, minValue);
 
-            Console.WriteLine("Трёхмерный массив случайных чисел от "+ (minValue + 1) + " до "+ (maxValue - 1) + ":");
+            Console.WriteLine("Трёхмерный массив случайных чисел от " + (minValue + 1) + " до " + (maxValue - 1) + ":");
             Console.WriteLine("");
             WriteArray(myArray);
 
             Console.WriteLine("Заменяем положительные элементы на нули:");
             Console.WriteLine("");
 
-            WriteArray(GetNoPositiveArray(myArray));
+            WriteArray(SetNoPositiveArray(myArray));
         }
 
         static void WriteArray(int[,,] myArray)
         {
             for (int x = 0; x <= 4; x++)
             {
-                WriteY(x, myArray);   
-            }                
+                WriteY(x, myArray);
+            }
         }
 
         static void WriteY(int x, int[,,] myArray)
         {
             for (int y = 0; y <= 4; y++)
             {
-                WriteZ(x, y, myArray);   
+                WriteZ(x, y, myArray);
             }
             Console.WriteLine();
         }
@@ -42,17 +42,17 @@ namespace _18NoPositive
             for (int z = 0; z <= 4; z++)
             {
                 Console.Write(myArray[x, y, z] + " ");
-            }               
+            }
             Console.WriteLine();
         }
 
-        static int[,,] GetNoPositiveArray(int[,,] myArray)
+        static int[,,] SetNoPositiveArray(int[,,] myArray)
         {
             for (int x = 0; x <= 4; x++)
             {
                 if (myArray[x, 0, 0] > 0)
                 {
-                    myArray[x, 0, 0] = 0;    
+                    myArray[x, 0, 0] = 0;
                 }
                 SetNoPositiveY(x, myArray);
             }
@@ -66,8 +66,8 @@ namespace _18NoPositive
             {
                 if (myArray[x, y, 0] > 0)
                 {
-                    myArray[x, y, 0] = 0;   
-                }                   
+                    myArray[x, y, 0] = 0;
+                }
                 SetNoPositiveZ(x, y, myArray);
             }
         }
@@ -78,8 +78,8 @@ namespace _18NoPositive
             {
                 if (myArray[x, y, z] > 0)
                 {
-                    myArray[x, y, z] = 0;    
-                }                    
+                    myArray[x, y, z] = 0;
+                }
             }
         }
 
@@ -88,17 +88,17 @@ namespace _18NoPositive
             int[,,] myArray = new int[5, 5, 5];
             Random myRandom = new Random();
 
-            for (int x = 0; x <= 4; i++)
+            for (int x = 0; x <= 4; x++)
             {
-                for (int y = 0; j <= 4; y++)
+                for (int y = 0; y <= 4; y++)
                 {
                     for (int z = 0; z <= 4; z++)
                     {
                         myArray[x, y, z] = myRandom.Next(minValue, maxValue);
-                    }                   
+                    }
                 }
             }
-                
+
             return myArray;
         }
     }
