e5d80b94fba67f38697e897684e66c18a3c6a489
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-02 21:04:53 +0400

    no message

--- Task01/1.11_AverageStringLength/111AverageStringLength/Program.cs
+++ Task01/1.11_AverageStringLength/111AverageStringLength/Program.cs
@@ -8,40 +8,39 @@ namespace _111AverageStringLength
         static void Main(string[] args)
         {
             Console.WriteLine("Введите строку. Максимальная длина - 254 симвоола");
-
-            //удаляем знаки препинания из строки
-            StringBuilder myStringBuilder = GetNoPunctuationSB(Console.ReadLine());
-
-            //копируем SB в новую строку
-            var fullString = myStringBuilder.ToString();
-            
-            //считаем общее количество символов в строке
-            var noSpaceStringLength = fullString.Replace(" ", "").Length;
-
-            //получаем массив слов
-            var wordsArray = myStringBuilder.ToString().Split(' ');
-
-            //средняя длина слова равна отношению количества символов строки к количеству слов
-            var result = (double)noSpaceStringLength / (double)wordsArray.Length;
-
             Console.WriteLine("");
-            Console.WriteLine("Средняя длина слова: "+ result);
+            Console.WriteLine("Средняя длина слова: " + GetAveradgeStringLength(Console.ReadLine()));
         }
 
-        static StringBuilder GetNoPunctuationSB(string userString)
+        static double GetAveradgeStringLength(string userString)
         {
-            StringBuilder myStringBuilder = new StringBuilder();
+            //удаляем все знаки препинания кроме пробелов
+            StringBuilder mySB = GetNoPuncExceptSpaceSB(userString);
+
+            //получаем массив слов
+            var wordsArray = mySB.ToString().Split(' ');
+
+            //удаляем пробелы
+            var noSpaceString = mySB.ToString().Replace(" ", "");
+
+            //средняя длина слова равна отношению количества символов в строке к количеству слов
+            return (double)noSpaceString.Length / (double)wordsArray.Length;
+        }
+
+        static StringBuilder GetNoPuncExceptSpaceSB(string userString)
+        {
+            StringBuilder mySB = new StringBuilder();
             var myCharArray = userString.ToCharArray();
 
             for (int i = 0; i <= myCharArray.Length - 1; i++)
             {
                 if (!Char.IsPunctuation(myCharArray[i]) | myCharArray[i] == ' ')
                 {
-                    myStringBuilder.Append(myCharArray[i]);
+                    mySB.Append(myCharArray[i]);
                 }
             }
 
-            return myStringBuilder;
+            return mySB;
         }
     }
 }
