4c67150413031cf92885ad38cb38de5344fc126c
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-03 23:41:03 +0400

    no message

--- Task01/1.12_CharDoubler/1.12_CharDoubler/Program.cs
+++ Task01/1.12_CharDoubler/1.12_CharDoubler/Program.cs
@@ -8,18 +8,18 @@ namespace _12_CharDoubler
         static void Main(string[] args)
         {
             Console.WriteLine("Максимальная длина строки - 254 симвоола");
-            Console.WriteLine("");
+            Console.WriteLine();
 
             Console.WriteLine("Введите первую строку:");
             var firstString = Console.ReadLine();
-            Console.WriteLine("");
+            Console.WriteLine();
 
             Console.WriteLine("Введите вторую строку:");
             var secondString = Console.ReadLine();
-            Console.WriteLine("");
+            Console.WriteLine();
 
-            Console.WriteLine("Результирующая строка: "+ GetCharDoubledString(firstString, secondString));
-            Console.WriteLine("");
+            Console.WriteLine($"Результирующая строка: {GetCharDoubledString(firstString, secondString)}");
+            Console.WriteLine();
         }
 
         static string GetCharDoubledString(string firstString, string secondString)
@@ -38,7 +38,7 @@ namespace _12_CharDoubler
                 }
             }
 
-            return mySB.ToString();
+            return mySB.ToString().Trim();
         }
 
     }
