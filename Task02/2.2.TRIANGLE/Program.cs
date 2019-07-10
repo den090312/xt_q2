6791ba6df48d1c78191eff01f6f226a6728763bd
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-10 22:29:00 +0400

    Добавил задачу 2.2 "Треугольник"

---
+++ Task02/2.2.TRIANGLE/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2._2.TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle myTriangle = new Triangle(3, 4, 5);
+
+            Console.WriteLine($"Периметр треугольника: {myTriangle.Perimeter}");
+        }
+
+        class Triangle
+        {
+            public int A { get; }
+            public int B { get; }
+            public int C { get; }
+
+            public int Perimeter => A + B + C;
+
+            public Triangle(int A, int B, int C)
+            {
+                if (A <= 0)
+                    ThrowException();
+                else
+                    this.A = A;
+
+                if (B <= 0)
+                    ThrowException();
+                else
+                    this.B = B;
+
+                if (C <= 0)
+                    ThrowException();
+                else
+                    this.C = C;
+            }
+
+            private void ThrowException() => throw new ArgumentException("Сторона треугольника не может быть меньше или равно нулю!");
+        }
+    }
+}
