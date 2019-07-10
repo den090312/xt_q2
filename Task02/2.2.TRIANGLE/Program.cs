d783e2f409851134bb7c78b6948b1532c25b20c9
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-10 22:57:38 +0400

    добавил проверку на существование треугольника

--- Task02/2.2.TRIANGLE/Program.cs
+++ Task02/2.2.TRIANGLE/Program.cs
@@ -19,29 +19,39 @@ namespace _2._2.TRIANGLE
             public int B { get; }
             public int C { get; }
 
+            public Triangle(int A, int B, int C)
+            {
+                int a;
+                if (A <= 0)
+                    throw new ArgumentException("Сторона треугольника не может быть меньше или равно нулю!");
+                else
+                    a = A;
+
+                int b;
+                if (B <= 0)
+                    throw new ArgumentException("Сторона треугольника не может быть меньше или равно нулю!");
+                else
+                    b = B;
+
+                int c;
+                if (C <= 0)
+                    throw new ArgumentException("Сторона треугольника не может быть меньше или равно нулю!");
+                else
+                    c = C;
+
+                if (a >= b + c | b >= a + c | c >= a + b)
+                    throw new ArgumentException("Треугольник с такими сторонами построить нельзя!");
+                else
+                {
+                    this.A = a;
+                    this.B = b;
+                    this.C = c;
+                }
+            }
+
             public int Perimeter => A + B + C;
 
             public double Area => Math.Sqrt((A + B + C) / 2 * ((A + B + C) / 2 - A) * ((A + B + C) / 2 - B) * ((A + B + C) / 2 - C));
-
-            public Triangle(int A, int B, int C)
-            {
-                if (A <= 0)
-                    ThrowException();
-                else
-                    this.A = A;
-
-                if (B <= 0)
-                    ThrowException();
-                else
-                    this.B = B;
-
-                if (C <= 0)
-                    ThrowException();
-                else
-                    this.C = C;
-            }
-
-            private void ThrowException() => throw new ArgumentException("Сторона треугольника не может быть меньше или равно нулю!");
         }
     }
 }
