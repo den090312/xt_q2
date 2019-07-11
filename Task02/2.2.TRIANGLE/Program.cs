b27735d831d3338d6aa6b46773d966ba7306a2ac
Denis Bolotin <31979965+den090312@users.noreply.github.com>
2019-07-11 11:01:17 +0400

    рефакторинг

--- Task02/2.2.TRIANGLE/Program.cs
+++ Task02/2.2.TRIANGLE/Program.cs
@@ -23,6 +23,66 @@ namespace _2._2.TRIANGLE
 
         class Triangle
         {
+            private int a, b, c;
+
+            public Triangle(int A, int B, int C)
+            {
+                int a = SetAboveZeroTriangleSide(A);
+                int b = SetAboveZeroTriangleSide(B);
+                int c = SetAboveZeroTriangleSide(C);
+
+                CheckTriangleExistence(a, b, c);
+
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+            public int A
+            {
+                get => a;
+                set
+                {
+                    a = SetAboveZeroTriangleSide(value);
+                    CheckTriangleExistence(a, b, c);
+                    a = value;
+                }
+            }
+
+            public int B
+            {
+                get => b;
+                set
+                {
+                    b = SetAboveZeroTriangleSide(value);
+                    CheckTriangleExistence(a, b, c);
+                    b = value;
+                }
+            }
+
+            public int Perimeter => A + B + C;
+
+            public double Area
+            {
+                get
+                {
+                    var halfPerimeter = (A + B + C) / 2;
+
+                    return Math.Sqrt(halfPerimeter * (halfPerimeter - A) * (halfPerimeter - B) * (halfPerimeter - C));
+                }
+            }
+
+            public int C
+            {
+                get => c;
+                set
+                {
+                    c = SetAboveZeroTriangleSide(value);
+                    CheckTriangleExistence(a, b, c);
+                    c = value;
+                }
+            }
+
             private static int SetAboveZeroTriangleSide(int userValue)
             {
                 if (userValue <= 0)
@@ -38,76 +98,6 @@ namespace _2._2.TRIANGLE
                 if (a >= b + c | b >= a + c | c >= a + b)
                 {
                     throw new ArgumentException("Треугольник с такими сторонами построить нельзя!");
-                }
-            }
-
-            private int a;
-            public int A
-            {
-                get => a;
-                set
-                {
-                    a = SetAboveZeroTriangleSide(value);
-
-                    if (a >= b + c | b >= a + c | c >= a + b)
-                    {
-                        throw new ArgumentException("Треугольник с такими сторонами построить нельзя!");
-                    }
-
-                    a = value;
-                }
-            }
-
-            private int b;
-            public int B
-            {
-                get => b;
-                set
-                {
-                    b = SetAboveZeroTriangleSide(value);
-
-                    CheckTriangleExistence(a, b, c);
-
-                    b = value;
-                }
-            }
-
-            private int c;
-            public int C
-            {
-                get => c;
-                set
-                {
-                    c = SetAboveZeroTriangleSide(value);
-
-                    CheckTriangleExistence(a, b, c);
-
-                    c = value;
-                }
-            }
-
-            public Triangle(int A, int B, int C)
-            {
-                int a = SetAboveZeroTriangleSide(A);
-                int b = SetAboveZeroTriangleSide(B);
-                int c = SetAboveZeroTriangleSide(C);
-
-                CheckTriangleExistence(a, b, c);
-
-                this.a = a;
-                this.b = b;
-                this.c = c;
-            }
-
-            public int Perimeter => A + B + C;
-
-            public double Area
-            {
-                get
-                {
-                    var halfPerimeter = (A + B + C) / 2;
-
-                    return Math.Sqrt(halfPerimeter * (halfPerimeter - A) * (halfPerimeter - B) * (halfPerimeter - C));
                 }
             }
         }
