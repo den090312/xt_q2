5fa0afb5be0ee7b88786ba507a49f7a93ef94381
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-11 19:11:45 +0400

    добавил консольные проверки для круга

--- Task02/2.1.ROUND/Program.cs
+++ Task02/2.1.ROUND/Program.cs
@@ -10,15 +10,18 @@ namespace _2._1.ROUND
             var myCoordinates = new Point(5, 20);
 
             //инициализируем радиус
-            double myRadius = 20;
+            Console.WriteLine("Введите радиус круга:");
+            bool itIsRadius = true;
+            double myRadius = GetIntFromConsole(itIsRadius);
 
             //создаем объект класса "Круг"
             var myRound = new Round(myCoordinates, myRadius);
 
             WriteRoundInfo(myRound);
 
-            //изменяем координаты         
-            myRound.СenterCoordinates = new Point(10, 10);
+            //изменяем координаты   
+            itIsRadius = false;
+            myRound.СenterCoordinates = new Point(GetIntFromConsole(itIsRadius), GetIntFromConsole(itIsRadius));
 
             //изменяем радиус
             myRound.Radius = 50;
@@ -33,11 +36,50 @@ namespace _2._1.ROUND
 
         private static void WriteRoundInfo(Round myRound)
         {
+            Console.WriteLine();
             Console.WriteLine($"Координаты центра круга: ({myRound.СenterCoordinates.X},{myRound.СenterCoordinates.Y})");
             Console.WriteLine($"Радиус круга: {myRound.Radius}");
             Console.WriteLine($"Длина окружности: {myRound.Circumference}");
             Console.WriteLine($"Площадь круга: {myRound.Area}");
             Console.WriteLine();
+        }
+
+        static int GetIntFromConsole(bool itIsRadius)
+        {
+            int intFromConsole;
+
+            bool isInt;
+
+            if (itIsRadius)
+            {
+                do
+                {
+                    Console.WriteLine($"Введите целое положительное число меньше или равно {int.MaxValue}:");
+                    isInt = int.TryParse(Console.ReadLine(), out intFromConsole);
+
+                    if (isInt)
+                    {
+                        isInt = intFromConsole > 0;
+                    }
+                }
+                while (isInt == false);
+            }
+            else
+            {
+                do
+                {
+                    Console.WriteLine($"Введите целое число меньше или равно {int.MaxValue}:");
+                    isInt = int.TryParse(Console.ReadLine(), out intFromConsole);
+
+                    if (isInt)
+                    {
+                        isInt = true;
+                    }
+                }
+                while (isInt == false);
+            }
+
+            return intFromConsole;
         }
     }
 }
