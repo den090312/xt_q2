6b904f36753d693281cdebc063dfa21059b7b5a7
Denis Bolotin <31979965+den090312@users.noreply.github.com>
2019-07-11 20:32:32 +0400

    задача 3

---
+++ Task02/2.3.USER/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2._3.USER
+{
+    class Program
+    {
+        private static void Main(string[] args) 
+        {
+
+        }
+    }
+}
---
+++ Task02/2.3.USER/User.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _2._3.USER
+{
+    public class User
+    {
+        private string firstName;
+        private string secondName;
+        private string lastName;
+        private DateTime birthDate;
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                CheckNameForNumbers(value);
+                firstName = value;
+            }
+        }
+        public string SecondName
+        {
+            get => secondName;
+            set
+            {
+                CheckNameForNumbers(value);
+                secondName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                CheckNameForNumbers(value);
+                lastName = value;
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get => birthDate;
+            set
+            {
+                if (value > DateTime.Now)
+                {
+                    throw new ArgumentException("Дата рождения не может быть больше текущей даты!");
+                }
+                birthDate = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                var userAge = DateTime.Now.AddYears(-birthDate.Year).Year;
+                if (userAge <= 0)
+                {
+                    throw new ArgumentException("Возраст не может быть отрицательным или меньше нуля!");
+                }
+                return userAge;
+            }
+        }
+
+        private static void CheckNameForNumbers(string userName)
+        {
+            var userCharArray = userName.ToCharArray();
+            foreach (char element in userCharArray)
+            {
+                if (char.IsDigit(element))
+                {
+                    throw new ArgumentException("Цифры в ФИО недопустимы!");
+                }
+            }
+        }
+    }
+}
