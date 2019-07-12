1073f5cce68297d48a0010a6449c4821e4b23208
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-12 21:06:52 +0400

    убрал пока лишнее поле

--- Task02/2.4.MY STRING/MyString.cs
+++ Task02/2.4.MY STRING/MyString.cs
@@ -4,24 +4,19 @@ namespace _2._4.MY_STRING
 {
     public class MyString
     {
-        private readonly int innerStringLength;
         private string innerString;
         private char[] innerCharArray;
 
         public MyString(string userString)
         {
             innerString = userString;
-            innerStringLength = userString.Length;
             innerCharArray = userString.ToCharArray();
         }
         public MyString(char[] userCharArray)
         {
             innerString = userCharArray.ToString();
-            innerStringLength = innerString.Length;
             innerCharArray = userCharArray;
         }
-
-        public int Length => innerStringLength;
 
         public char[] MyStringToCharArray(MyString myString)
         {
