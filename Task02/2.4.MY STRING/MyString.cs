2d46ac844d236d18a06ac383a42e1c53efa17491
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-12 21:05:17 +0400

    Методы, конструкторы, перегрузки

--- Task02/2.4.MY STRING/MyString.cs
+++ Task02/2.4.MY STRING/MyString.cs
@@ -4,9 +4,37 @@ namespace _2._4.MY_STRING
 {
     public class MyString
     {
-        MyString(string userString)
-        {
+        private readonly int innerStringLength;
+        private string innerString;
+        private char[] innerCharArray;
 
+        public MyString(string userString)
+        {
+            innerString = userString;
+            innerStringLength = userString.Length;
+            innerCharArray = userString.ToCharArray();
+        }
+        public MyString(char[] userCharArray)
+        {
+            innerString = userCharArray.ToString();
+            innerStringLength = innerString.Length;
+            innerCharArray = userCharArray;
+        }
+
+        public int Length => innerStringLength;
+
+        public char[] MyStringToCharArray(MyString myString)
+        {
+            innerCharArray = myString.innerString.ToCharArray();
+
+            return innerCharArray;
+        }
+
+        public MyString CharArrayToMyString(char[] userCharArray)
+        {
+            MyString myString = new MyString(userCharArray);
+
+            return myString;
         }
     }
 }
