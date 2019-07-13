b8a3dd4a5ddaa76bf7c73099b1c1673d0d6a93ee
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-13 17:51:52 +0400

    класс "окружность"

---
+++ Task02/2.1.ROUND/Circle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _2._1.ROUND
+{
+    public class Circle
+    {
+        public Point СenterCoordinates = new Point(0, 0);
+
+        //длина окружности
+        public double Circumference => 2 * Math.PI * Radius;
+     
+        private double radius;
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                CheckRadiusAboveZero(value);
+                radius = value;
+            }
+        }
+
+        //конструктор
+        public Circle(Point userCenterCoordinates, double userRadius)
+        {
+            CheckRadiusAboveZero(userRadius);
+            СenterCoordinates = userCenterCoordinates;
+            radius = userRadius;
+        }
+
+        private static void CheckRadiusAboveZero(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Отрицательное значение радиуса недопустимо!");
+            }
+        }
+    }
+}
--- Task02/2.1.ROUND/Round.cs
+++ Task02/2.1.ROUND/Round.cs
@@ -2,45 +2,14 @@
 
 namespace _2._1.ROUND
 {
-    //класс "Круг"
-    public class Round
+    public class Round : Circle
     {
-        //инициализируем координаты центра круга
-        public Point СenterCoordinates = new Point(0, 0);
+        public Round(Point userCenterCoordinates, double userRadius) : base(userCenterCoordinates, userRadius)
+        {
+        }
 
-        //длина окружности
-        public double Circumference => 2 * Math.PI * Radius;
-
-        //площадь круга
         public double Area => Math.PI * Radius * Radius;
 
-        //радиус круга            
-        private double radius;
 
-        public double Radius
-        {
-            get => radius;
-            set
-            {
-                CheckRadiusAboveZero(value);
-                radius = value;
-            }
-        }
-
-        //конструктор
-        public Round(Point userCenterCoordinates, double userRadius)
-        {
-            CheckRadiusAboveZero(userRadius);
-            СenterCoordinates = userCenterCoordinates;
-            radius = userRadius;
-        }
-
-        private static void CheckRadiusAboveZero(double radius)
-        {
-            if (radius <= 0)
-            {
-                throw new ArgumentException("Отрицательное значение радиуса недопустимо!");
-            }
-        }
     }
 }
