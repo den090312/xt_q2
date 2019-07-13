a6deea672426a76427c2c335515cededcf208bf7
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-13 21:54:25 +0400

    линия и прямоугольник

--- Task02/2.7.VECTOR GRAPHICS EDITOR/Program.cs
+++ Task02/2.7.VECTOR GRAPHICS EDITOR/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _2._1.ROUND;
+using System;
 using System.Text;
 
 namespace _2._7.VECTOR_GRAPHICS_EDITOR
@@ -17,7 +18,6 @@ namespace _2._7.VECTOR_GRAPHICS_EDITOR
                 if (userKey != 0)
                 {
                     CreateSelectedFigure(userKey);
-                    Console.WriteLine("Выберите фигуру для вывода:");
                     WriteMenu();
                 }
             }
@@ -32,6 +32,30 @@ namespace _2._7.VECTOR_GRAPHICS_EDITOR
             Console.WriteLine("\t4: Круг");
             Console.WriteLine("\t5: Кольцо");
             Console.WriteLine("\t6: ВЫХОД");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static TwoPointsFigure GetTwoPointsFigureFromConsole()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Точка 1");
+            var point1 = new Point(GetCoordinateFromConsole('X'), GetCoordinateFromConsole('Y'));
+            Console.WriteLine();
+            Console.WriteLine("Точка 2");
+            var point2 = new Point(GetCoordinateFromConsole('X'), GetCoordinateFromConsole('Y'));
+            Console.WriteLine();
+            var userFigure = new TwoPointsFigure(point1, point2);
+
+            return userFigure;
+        }
+
+        private static void DisplayTwoPointsFigure(string figureType, TwoPointsFigure twoPointsFigure)
+        {
+            Console.WriteLine($"Тип фигуры: {figureType}");
+            Console.WriteLine($"Точка1: ({twoPointsFigure.Point1.X},{twoPointsFigure.Point1.Y})");
+            Console.WriteLine($"Точка2: ({twoPointsFigure.Point2.X},{twoPointsFigure.Point2.Y})");
+            Console.WriteLine();
         }
 
         private static void CreateSelectedFigure(int consoleKey)
@@ -39,10 +63,12 @@ namespace _2._7.VECTOR_GRAPHICS_EDITOR
             switch (consoleKey)
             {
                 case 1:
+                    DisplayTwoPointsFigure("Линия", GetTwoPointsFigureFromConsole());
                     break;
                 case 2:
                     break;
                 case 3:
+                    DisplayTwoPointsFigure("Прямоугольник", GetTwoPointsFigureFromConsole());
                     break;
                 case 4:
                     break;
@@ -52,6 +78,22 @@ namespace _2._7.VECTOR_GRAPHICS_EDITOR
                     Environment.Exit(0);
                     break;
             }
+        }
+
+        private static int GetCoordinateFromConsole(char measure)
+        {
+            int intFromConsole;
+            bool isInt;
+
+            do
+            {
+                Console.WriteLine($"Ввод координаты. Измерение {measure}");
+                Console.WriteLine($"Введите целое число меньше или равно {int.MaxValue}:");
+                isInt = int.TryParse(Console.ReadLine(), out intFromConsole);
+            }
+            while (isInt == false);
+
+            return intFromConsole;
         }
 
         static int GetKeyFromConsole()
