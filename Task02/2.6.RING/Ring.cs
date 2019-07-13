414a9731d75b1109d8f960f711cecc1bb329ecc2
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-13 18:49:57 +0400

    площадь кольца

--- Task02/2.6.RING/Ring.cs
+++ Task02/2.6.RING/Ring.cs
@@ -6,6 +6,7 @@ namespace _2._6.RING
     class Ring : Circle
     {
         private double ringRadius;
+        private double ringArea;
 
         public double RingRadius
         {
@@ -21,6 +22,14 @@ namespace _2._6.RING
 
                 ringRadius = value;
             }
+        }
+
+        public double RingArea
+        {
+            get => ringArea;
+            private set => ringArea = RingRadius > Radius
+                    ? Math.PI * (RingRadius * RingRadius - Radius * Radius)
+                    : Math.PI * (Radius * Radius - RingRadius * RingRadius);
         }
 
         public double RingCircumference => 2 * Math.PI * RingRadius;
