7c29e5a840a7a0466520d5b493750b96b676ab3d
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-15 20:53:43 +0400

    добавил для поля конструктор

--- Task02/2.8.GAME/Program.cs
+++ Task02/2.8.GAME/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _2._8.GAME
+﻿using System;
+
+namespace _2._8.GAME
 {
     public class Program
     {
@@ -11,6 +13,22 @@
         {
             private int width;
             private int height;
+
+            private Field(int fieldWidth, int fieldHeight)
+            {
+                if (fieldWidth < 0)
+                {
+                    throw new Exception("Ширина поля не может быть меньше нуля!");
+                }
+
+                if (fieldHeight < 0)
+                {
+                    throw new Exception("Высота поля не может быть меньше нуля!");
+                }
+
+                width = fieldWidth;
+                height = fieldHeight;
+            }
         }
 
         public abstract class Subject
