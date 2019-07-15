1ca980956762030289726df7537eda9e348dee97
Денис Болотин <den090312@gmail.com>
2019-07-15 11:37:25 +0400

    наметки классов

--- Task02/2.8.GAME/Program.cs
+++ Task02/2.8.GAME/Program.cs
@@ -6,5 +6,38 @@
         {
 
         }
+
+        public class Field
+        {
+            private int width;
+            private int height;
+        }
+
+        public abstract class Subject
+        {
+            private readonly bool movable;
+        }
+
+        public class Player : Subject
+        {
+            private readonly bool movable = true;
+            private int health;
+            private int speed;
+        }
+
+        public class Bonus : Subject
+        {
+            private readonly bool movable = false;
+        }
+
+        public class Monster : Subject
+        {
+            private readonly bool movable = true;
+        }
+
+        public class Obstruction : Subject
+        {
+            private readonly bool movable = false;
+        }
     }
 }
