c1b04c8681c2e2c48ba5f38c2b55564b211bd576
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-20 00:47:23 +0400

    LOST

---
+++ Task03/31_LOST/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace _31_LOST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+
+            var n = GetPositiveIntFromConsole();
+
+            for (int i = 1; i <= n; i++)
+            {
+                numbers.Enqueue(i);
+            }
+
+            //пока работает только для четного числа
+            n = 1;
+
+            if (numbers.Count % 2 == 0)
+            {
+                do
+                {
+                    if (n == 2)
+                    {
+                        numbers.Dequeue();
+                        n = 1;
+                    }
+                    else
+                    {
+                        numbers.Enqueue(numbers.Peek());
+                        numbers.Dequeue();
+                        n = 2;
+                    }
+                }
+                while (numbers.Count > 1);
+            }
+            else
+            {
+                do
+                {
+                    if (n == 2)
+                    {
+                        numbers.Dequeue();
+                        n = 1;
+                    }
+                    else
+                    {
+                        numbers.Enqueue(numbers.Peek());
+                        numbers.Dequeue();
+                        n = 2;
+                    }
+                }
+                while (numbers.Count > 1);
+            }
+
+            Console.WriteLine(numbers.Peek());
+
+        }
+
+        static int GetPositiveIntFromConsole()
+        {
+            Console.WriteLine("Количество людей в кругу");
+
+            int manQuantity;
+
+            bool isInt;
+            do
+            {
+                Console.WriteLine($"Введите положительное целое число меньше или равно {int.MaxValue}");
+                isInt = int.TryParse(Console.ReadLine(), out manQuantity);
+
+                if (isInt)
+                {
+                    isInt = manQuantity > 0;
+                }
+            }
+            while (isInt == false);
+
+            return manQuantity;
+        }
+    }
+}
