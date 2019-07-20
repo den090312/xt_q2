346776590807a4272e0bc982860a7fa49e225bc0
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-21 02:07:47 +0400

    предварительное решение

--- Task03/32_WORD FREQUENCY/Program.cs
+++ Task03/32_WORD FREQUENCY/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace _32_WORD_FREQUENCY
@@ -8,13 +9,67 @@ namespace _32_WORD_FREQUENCY
         private static void Main(string[] args)
         {
             Console.WriteLine("Введите английский текст");
-            var wordsArray = GetEnglishTextFromConsole().ToLower().Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            var wordsArray = GetEnglishTextFromConsole().Split(new char[] { ' ', '.' }, StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (string word in wordsArray)
+            var wordsList = new List<string>();
+
+            for (int i = 0; i < wordsArray.Length; i++)
             {
+                var word = GetNoPunctuationWord(wordsArray[i]);
 
+                if (word.Length > 0)
+                {
+                    wordsList.Add(word);
+                }
             }
 
+            var wordDict = new Dictionary<string, int>();
+
+            foreach (string word in wordsList)
+            {
+                if (!wordDict.ContainsKey(word))
+                {
+                    wordDict.Add(word, GetFrequency(word, wordsList));
+                }
+            }
+
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, int> kvp in wordDict)
+            {
+                Console.WriteLine($"Слово \"{kvp.Key}\" встречается {kvp.Value} раз(а)");
+            }
+        }
+
+        private static int GetFrequency(string word, List<string> wordsList)
+        {
+            int frequency = 0;
+
+            foreach (string element in wordsList)
+            {
+                if (element.ToLower() == word.ToLower())
+                {
+                    frequency++;
+                }
+            }
+
+            return frequency;
+        }
+
+        private static string GetNoPunctuationWord(string word)
+        {
+            StringBuilder mySB = new StringBuilder();
+            var myCharArray = word.ToCharArray();
+
+            for (int i = 0; i <= myCharArray.Length - 1; i++)
+            {
+                if (!Char.IsPunctuation(myCharArray[i]))
+                {
+                    mySB.Append(myCharArray[i]);
+                }
+            }
+
+            return mySB.ToString();
         }
 
         private static string GetEnglishTextFromConsole()
