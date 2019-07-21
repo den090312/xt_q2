7f303d2f7902cbc07d8e344731b2c7e680e36763
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-21 13:01:49 +0400

    класс

---
+++ Task03/33_DYNAMIC ARRAY/DynamicArray.cs
@@ -0,0 +1,8 @@
+﻿
+namespace _33_DYNAMIC_ARRAY
+{
+    class DynamicArray<T>
+    {
+        private char[] charArray = new char[0]; 
+    }
+}
--- Task03/33_DYNAMIC ARRAY/Program.cs
+++ Task03/33_DYNAMIC ARRAY/Program.cs
@@ -1,11 +1,12 @@
-﻿
+﻿using System;
+
 namespace _33_DYNAMIC_ARRAY
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Do something");
         }
     }
 }
