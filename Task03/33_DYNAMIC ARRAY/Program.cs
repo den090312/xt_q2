424268359969d5d272741868661c7749e397b583
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-21 23:41:26 +0400

    проверки

--- Task03/33_DYNAMIC ARRAY/Program.cs
+++ Task03/33_DYNAMIC ARRAY/Program.cs
@@ -8,16 +8,17 @@ namespace _33_DYNAMIC_ARRAY
     {
         static void Main(string[] args)
         {
-            var myList1 = new List<string>() { "0", "2", "3", "1", "5", "4" };
+            //var myList1 = new List<string>() { "0", "2", "3", "1", "5", "4" };
+            var myList1 = new List<string>() { };
 
             var MyDA = new DynamicArray<string>(myList1);
 
             //MyDA.Insert("751", 3);
-            //MyDA.Remove("5");
-            //MyDA.Add("321");
+            //MyDA.Remove("2");
+            MyDA.Add("321");
 
-            var myList2 = new List<string>(new string[6] { "0", "2", "3", "1", "5", "4" });
-            MyDA.AddRange(myList2);
+            //var myList2 = new List<string>(new string[6] { "0", "2", "3", "1", "5", "4" });
+            //MyDA.AddRange(myList2);
 
             foreach (string element in MyDA)
             {
