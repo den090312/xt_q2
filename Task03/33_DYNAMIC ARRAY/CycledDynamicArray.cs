8ac77c21574955286314908da7bb4340c6985175
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-22 22:37:19 +0400

    CycledDynamicArray start

---
+++ Task03/33_DYNAMIC ARRAY/CycledDynamicArray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace _33_DYNAMIC_ARRAY
+{
+    public class CycledDynamicArray<T> : IEnumerable, IEnumerable<T>
+    {
+        private DynamicArray<T> dynamicArray;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)dynamicArray.ToArray()).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)dynamicArray.ToArray()).GetEnumerator();
+        }
+    }
+}
