ebe31106f4f6b7bade10ce89b8383cb9918bc383
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-22 18:32:41 +0400

    1.3 Square is back!

--- Task00/1.3.SQUARE/Program.cs
+++ Task00/1.3.SQUARE/Program.cs
@@ -4,26 +4,23 @@ namespace _1._3.SQUARE
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            if (ItsSimpleInt(GetPositiveIntFromConsole()))
-                Console.WriteLine("Простое число");
-            else
-                Console.WriteLine("Составное число");
+            DrawSquare(GetPositiveOddIntFromConsole());
         }
 
-        static int GetPositiveIntFromConsole()
+        private static int GetPositiveOddIntFromConsole()
         {
             int n;
             bool isInt;
             do
             {
-                Console.WriteLine("Введите положительное число больше 1 и меньше 2147483648");
+                Console.WriteLine("Введите нечетное число больше 2 и меньше 2147483648");
                 isInt = int.TryParse(Console.ReadLine(), out n);
 
                 if (isInt)
                 {
-                    isInt = n > 1;
+                    isInt = n > 2 & n % 2 != 0;
                 }
             }
             while (isInt == false);
@@ -31,47 +28,26 @@ namespace _1._3.SQUARE
             return n;
         }
 
-        static bool ItsSimpleInt(int n)
+        private static void DrawSquare(int n)
         {
-            //четное число - всегда составное (кроме 2)
-            if (n % 2 == 0)
-            {
-                return n == 2;
-            }
+            int centralPoint = (n - 1) / 2;
 
-            //если можно извлечь кв. корень без остатка - число составное
-            double dSqrt = Math.Sqrt(n);
-            if (int.TryParse(dSqrt.ToString(), out int X))
+            for (int i = 0; i < n; i++)
             {
-                return false;
-            }
-
-            //особенности округления при конвертации double в int - к положительным числам прибавить 0.5
-            //должно работать быстрее, чем Math.Round() и Convert.ToInt32()
-            int M = (int)(dSqrt + 0.5);
-            //int N = DoubleToIntRound(dSqrt);
-
-            //проверяем результат от деления на нечетные числа от 3 до округленного кв. корня
-            for (int i = 3; i <= M; i += 2)
-            {
-                if (n % i == 0)
+                for (int j = 0; j < n; j++)
                 {
-                    return false;
+                    if (i == centralPoint & j == centralPoint)
+                    {
+                        Console.Write(' ');
+                    }
+                    else
+                    {
+                        Console.Write('*');
+                    }
                 }
-            }
 
-            return true;
+                Console.WriteLine();
+            }
         }
-
-        //метод округления при конвертации double в int
-        //использовать не стал, потому что отрицательные числа отсекаются на этапе проверки ввода
-        static int DoubleToIntRound(double d) => d < 0 ? (int)(d - 0.5) : (int)(d + 0.5);
-        /*{
-            if (d < 0)
-            {
-                return (int)(d - 0.5);
-            }
-            return (int)(d + 0.5);
-        }*/
     }
 }
