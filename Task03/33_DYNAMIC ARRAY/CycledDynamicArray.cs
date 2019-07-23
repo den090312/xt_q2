02c4385f122c2645165d5efe2a7f32ea39f5d396
Денис Болотин <den090312@gmail.com>
2019-07-23 11:36:20 +0400

    fix

--- Task03/33_DYNAMIC ARRAY/CycledDynamicArray.cs
+++ Task03/33_DYNAMIC ARRAY/CycledDynamicArray.cs
@@ -1,43 +1,21 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 
 namespace _33_DYNAMIC_ARRAY
 {
-    public class CycledDynamicArray<T> : IEnumerable, IEnumerable<T>, IEnumerator
+    public class CycledDynamicArray<T> : IEnumerable
     {
-        private DynamicArray<T> baseDynamicArray;
+        private readonly DynamicArray<T> baseDynamicArray;
 
-        int position = -1;
-
-        public CycledDynamicArray(DynamicArray<T> userDynamicArray)
-        {
-            baseDynamicArray = userDynamicArray;
-        }
-
-        public object Current => Current;
-
-        public bool MoveNext()
-        {
-            position++;
-
-            if (position >= baseDynamicArray.Length)
-            {
-                position = 0;
-            }
-
-            return (position < baseDynamicArray.Length);
-        }
-
-        public void Reset() => position = -1;
+        public CycledDynamicArray(DynamicArray<T> userDynamicArray) => baseDynamicArray = userDynamicArray;
 
         public IEnumerator GetEnumerator()
         {
-            return ((IEnumerable)baseDynamicArray).GetEnumerator();
-        }
+            if (!GetEnumerator().MoveNext())
+            {
+                GetEnumerator().Reset();
+            }
 
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            return ((IEnumerable<T>)baseDynamicArray).GetEnumerator();
+            return baseDynamicArray.GetEnumerator();
         }
     }
 }
