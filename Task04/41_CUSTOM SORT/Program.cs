d681c5e61575a50377eaeacceb1e0385add48b0d
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-25 21:22:07 +0400

    пробы пера)

--- Task04/41_CUSTOM SORT/Program.cs
+++ Task04/41_CUSTOM SORT/Program.cs
@@ -1,11 +1,26 @@
 ﻿
+using System;
+
 namespace _41_CUSTOM_SORT
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var myArray = new string[]{ "1", "3", "0", "7", "-90", "123" };
 
+            Sorter<string> sorter = new Sorter<string>(Sort);
+
+            sorter(myArray);
+        }
+
+        public delegate T[] Sorter<T>(T[] array);
+
+        public static T[] Sort<T>(T[] array)
+        {
+            Array.Sort(array);
+
+            return array;
         }
     }
 }
