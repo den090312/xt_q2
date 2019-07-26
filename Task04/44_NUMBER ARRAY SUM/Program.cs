e7a5ac1fb191df085d56ff3b8adc607b24c1e418
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-27 01:53:15 +0400

    added func

--- Task04/44_NUMBER ARRAY SUM/Program.cs
+++ Task04/44_NUMBER ARRAY SUM/Program.cs
@@ -9,17 +9,21 @@ namespace _44_NUMBER_ARRAY_SUM
         {
             var myArray = new int[] { 1, 3, 0, 7, 9, 5 };
 
-            Console.WriteLine(GetSum.Sum(myArray));
+            Func<int[], Type> type = GetType;
+
+            Console.WriteLine(GetSum.Sum(myArray, type));
         }
+
+        public static Type GetType<T>(T[] array) => array.GetType();
     }
 
     internal static class GetSum
     {
-        public static T Sum<T>(T[] array)
+        public static T Sum<T>(T[] array, Func<T[], Type> type)
         {
             var arrayType = array.GetType();
 
-            T sum;
+            T sum = 0;
 
             foreach (T item in array)
             {
