65c9f0c469e2d4521284e265c42bae83ee91bcd1
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-27 01:15:51 +0400

    4.4. NUMBER ARRAY SUM start

---
+++ Task04/44_NUMBER ARRAY SUM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _44_NUMBER_ARRAY_SUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
