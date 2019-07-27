afa9dfd587da86e4dc72f8a181f91f4cdeacf6bb
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-27 13:32:47 +0400

    45 start

---
+++ Task04/45_TO INT OR NOT TO INT/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _45_TO_INT_OR_NOT_TO_INT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    internal static class StringExtension
+    {
+        internal static bool IsInt(this string userString)
+        {
+            return true;
+        }
+    }
+}
