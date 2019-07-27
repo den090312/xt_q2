b3fea8be13748486b83725835425a15ef293b1cf
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-27 11:37:16 +0400

    try decimal array finish

--- Task04/44_NUMBER ARRAY SUM/Program.cs
+++ Task04/44_NUMBER ARRAY SUM/Program.cs
@@ -9,25 +9,27 @@ namespace _44_NUMBER_ARRAY_SUM
         {
             var myArray = new int[] { 1, 3, 0, 7, 9, 5 };
 
-            Func<int[], decimal[]> toDecimalArray = ToDecimalArray;
+            Func<int[], decimal[]> decimalArray = GetDecimalArray;
 
-            var sum = GetSum.Sum(myArray, toDecimalArray);
+            var sum = GetSum.Sum(myArray, decimalArray);
 
             Console.WriteLine(sum);
         }
 
-        public static decimal[] ToDecimalArray<T>(T array) => (decimal[])(object)array;
+        public static decimal[] GetDecimalArray<T>(T array) => (decimal[])(object)array;
     }
 
     internal static class GetSum
     {
-        public static decimal Sum<T>(T[] array, Func<int[], decimal[]> toDecimalArray)
+        public static decimal Sum<T>(T[] array, Func<T[], decimal[]> getDecimalArray)
         {
+            var newArray = getDecimalArray(array);
+
             decimal sum = 0;
 
-            for (int i = 0; i < array.Length; i++)
+            for (int i = 0; i < newArray.Length; i++)
             {
-                sum += array[i];
+                sum += newArray[i];
             }
 
             return sum;
