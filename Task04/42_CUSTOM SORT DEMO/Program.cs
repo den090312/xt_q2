9ee9203e47ff0269d85511119e44ab731cf204db
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-28 02:43:13 +0400

    refs fix

--- Task04/42_CUSTOM SORT DEMO/Program.cs
+++ Task04/42_CUSTOM SORT DEMO/Program.cs
@@ -4,37 +4,14 @@ namespace _42_CUSTOM_SORT_DEMO
 {
     public class Program
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var myArray = new string[] { "abcdi", "dcdef", "aby", "abc", "abcdef", "abcdez" };
+            var stringArray = new string[] { "abcdi", "dcdef", "aby", "abc", "abcdef", "abcdez" };
 
-            Func<string, string, bool> myComparer = Compare;
+            Func<string, string, bool> stringComparer = Compare;
 
-            Sort(myArray, myComparer);
-
-            foreach (string item in myArray)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        private static T[] Sort<T>(T[] array, Func<T, T, bool> compare)
-        {
-            _41_CUSTOM_SORT.Program.NullCheck(array);
-            _41_CUSTOM_SORT.Program.NullCheck(compare);
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (compare(array[j], array[i]))
-                    {
-                        _41_CUSTOM_SORT.Program.Swap(ref array[i], ref array[j]);
-                    }
-                }
-            }
-
-            return array;
+            _41_CUSTOM_SORT.Program.Sort(stringArray, stringComparer);
+            _41_CUSTOM_SORT.Program.WriteArray(stringArray);
         }
 
         private static bool Compare<T>(T x, T y)
