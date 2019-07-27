30140dc256667102641845c2c5a1dfb33070c447
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-27 22:58:09 +0400

    anon

--- Task04/46_I SEEK YOU/Program.cs
+++ Task04/46_I SEEK YOU/Program.cs
@@ -14,16 +14,19 @@ namespace _46_I_SEEK_YOU
 
             Func<int, bool> condition = IsPositive;
 
-            var allPositiveArray1 = SeachInArrayExtansion.ArrayFindAll(myArray, condition);
+            var allPositiveArray1 = ArrayFindAllFunc(myArray, condition);
 
-            var allPositiveArray2 = SeachInArrayExtansion.ArrayFindAll(myArray, x => x > 0);
+            Condition<int> positive = delegate (int userInt) { return userInt > 0; };
+
+            var allPositiveArray2 = ArrayFindAllAnon(myArray, positive);
+
+            var allPositiveArray3 = ArrayFindAllFunc(myArray, x => x > 0);
         }
 
         public static bool IsPositive(int userInt) => userInt > 0;
-    }
 
-    internal static class SeachInArrayExtansion
-    {
+        public delegate bool Condition<T>(T userPar);
+
         public static bool ArrayTryFind<T>(T[] array, T userElement, out int index)
         {
             index = 0;
@@ -44,7 +47,22 @@ namespace _46_I_SEEK_YOU
             return found;
         }
 
-        public static T[] ArrayFindAll<T>(T[] array, Func<T, bool> condition)
+        public static T[] ArrayFindAllFunc<T>(T[] array, Func<T, bool> condition)
+        {
+            var tempList = new List<T>();
+
+            foreach (T element in array)
+            {
+                if (condition(element))
+                {
+                    tempList.Add(element);
+                }
+            }
+
+            return tempList.ToArray();
+        }
+
+        public static T[] ArrayFindAllAnon<T>(T[] array, Condition<T> condition)
         {
             var tempList = new List<T>();
 
@@ -60,3 +78,4 @@ namespace _46_I_SEEK_YOU
         }
     }
 }
+
