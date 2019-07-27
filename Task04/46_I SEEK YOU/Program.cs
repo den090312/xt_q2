1b0e5d4e60f795435c775e1fb2f9c8c35c9b6201
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-27 14:16:18 +0400

    4.6. I SEEK YOU start

---
+++ Task04/46_I SEEK YOU/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _46_I_SEEK_YOU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
