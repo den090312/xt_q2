c5955ba11bdb526c6609e7052b2c7433a0bf7d6f
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-30 20:32:02 +0400

    class Watcher

--- Task05/51_BACKUP SYSTEM/Storage.cs
+++ Task05/51_BACKUP SYSTEM/Storage.cs
@@ -9,7 +9,7 @@ namespace _51_BACKUP_SYSTEM
     public abstract class Storage
     {
         private static readonly string root = @"D:\Storage";
-        private static readonly string logDir = @"D:\log.txt";
+        private static readonly string logDir = @"D:\log.xml";
 
         public virtual string FullName { get; protected set; }
     }
