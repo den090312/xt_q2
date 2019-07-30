2ba75ab1f8a7bcc955935ab25c61557120bcdc20
den090312 <31979965+den090312@users.noreply.github.com>
2019-07-30 20:02:19 +0400

    class Storage

---
+++ Task05/51_BACKUP SYSTEM/Storage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51_BACKUP_SYSTEM
+{
+    public abstract class Storage
+    {
+        private readonly string storagePath = @"D:\Storage";
+        private readonly string logFile = @"D:\log.txt";
+    }
+}
