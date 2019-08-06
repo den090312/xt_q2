759e64f2e30c51f32ec8a801c4c417b79fbfee85
den090312 <31979965+den090312@users.noreply.github.com>
2019-08-06 21:47:31 +0400

    watcher fix

--- Task05/51_BACKUP SYSTEM/Watcher.cs
+++ Task05/51_BACKUP SYSTEM/Watcher.cs
@@ -5,7 +5,7 @@ using System.Threading;
 
 namespace _51_BACKUP_SYSTEM
 {
-    public abstract class Watcher
+    public class Watcher
     {
         private int lastRead = DateTime.MinValue.Millisecond;
 
