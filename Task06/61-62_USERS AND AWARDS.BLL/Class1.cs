d14baea6c2d405a9629bbceb76a5367c8f2408b0
den090312 <31979965+den090312@users.noreply.github.com>
2019-08-13 21:20:51 +0400

    Task06 start

---
+++ Task06/61-62_USERS AND AWARDS.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _61_62_USERS_AND_AWARDS.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ Task06/61-62_USERS AND AWARDS.PL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _61_62_USERS_AND_AWARDS.PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
