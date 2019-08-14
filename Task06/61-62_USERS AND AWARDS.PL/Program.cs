cc3823f54b0e6632924ec26e5b2a89ed9ed14b08
den090312 <31979965+den090312@users.noreply.github.com>
2019-08-14 20:49:04 +0400

    PL improvements

--- Task06/61-62_USERS AND AWARDS.PL/Program.cs
+++ Task06/61-62_USERS AND AWARDS.PL/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _61_62_USERS_AND_AWARDS.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,33 @@ namespace _61_62_USERS_AND_AWARDS.PL
     {
         private static void Main(string[] args)
         {
+            Storage.Create();
+            Storage.WriteInfo();
 
+            WriteMenu();
+
+            bool inputComplete = false;
+
+            while (!inputComplete)
+            {
+                int userKey = GetKeyFromConsole();
+
+                if (userKey != 0)
+                {
+                    switch (userKey)
+                    {
+                        case 1:
+                            inputComplete = true;
+                            break;
+                        case 2:
+                            inputComplete = true;
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+            }
         }
 
         private static void WriteMenu()
@@ -19,6 +46,62 @@ namespace _61_62_USERS_AND_AWARDS.PL
             Console.WriteLine("\t1: create");
             Console.WriteLine("\t2: delete");
             Console.WriteLine("\t3: exit");
+        }
+
+        private static int GetKeyFromConsole()
+        {
+            bool inputComplete = false;
+
+            StringBuilder userKeySB = new StringBuilder();
+
+            while (!inputComplete)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    EmulateConsoleKeyBackSpace(userKeySB);
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    inputComplete = true;
+                }
+                else if (char.IsDigit(key.KeyChar) & (key.KeyChar == '1' || key.KeyChar == '2' || key.KeyChar == '3'))
+                {
+                    if (userKeySB.Length < 1)
+                    {
+                        userKeySB.Append(key.KeyChar);
+                        Console.Write(key.KeyChar);
+                    }
+                }
+            }
+
+            int result;
+
+            if (userKeySB.Length > 0)
+            {
+                result = int.Parse(userKeySB.ToString());
+            }
+            else
+            {
+                result = 0;
+            }
+
+            return result;
+        }
+
+        static void EmulateConsoleKeyBackSpace(StringBuilder userKeySB)
+        {
+            if (userKeySB.Length > 0)
+            {
+                userKeySB.Length--;
+            }
+
+            Console.Clear();
+            Storage.WriteInfo();
+            WriteMenu();
+
+            Console.Write(userKeySB);
         }
     }
 }
