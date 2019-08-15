f8e851a53cf9b2566efc0342b0731324dde7ce81
Денис Болотин <den090312@gmail.com>
2019-08-15 10:06:10 +0400

    User, Award implement start

--- Task06/61-62_USERS AND AWARDS.Entities/Award.cs
+++ Task06/61-62_USERS AND AWARDS.Entities/Award.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using _61_62_USERS_AND_AWARDS.Interfaces;
 
 namespace _61_62_USERS_AND_AWARDS.Entities
 {
-    class Award
+    class Award : IStorable, IDeleteble
     {
+        public void PrintObjects()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Create()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
