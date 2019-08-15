d09f138d0f54e73da79deaf03aa633115b53ff7b
Денис Болотин <den090312@gmail.com>
2019-08-15 16:32:24 +0400

    User refactor

--- Task06/61-62_USERS AND AWARDS.Entities/User.cs
+++ Task06/61-62_USERS AND AWARDS.Entities/User.cs
@@ -1,18 +1,47 @@
 ﻿using _61_62_USERS_AND_AWARDS.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace _61_62_USERS_AND_AWARDS.Entities
 {
     class User : IStorable, IDeleteble
     {
+        public readonly string DateFormat = "dd.MM.yyyy";
         private readonly DateTime currentDateTime = DateTime.Now.Date;
+        private string name = string.Empty;
+        private DateTime dateOfBirth;
 
-        public string Id { get; } = string.Empty; 
+        public string Id { get; } = string.Empty;
 
-        public string Name { get; } = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                CheckName(value, "Имя");
+                name = value;
+            }
+        }
 
-        public DateTime DateOfBirth { get; }
+        public DateTime DateOfBirth
+        {
+            get => dateOfBirth;
+            set
+            {
+                if (value > currentDateTime)
+                {
+                    throw new ArgumentException("Дата рождения не может быть больше текущей даты!");
+                }
+
+                if (value == currentDateTime)
+                {
+                    throw new ArgumentException("Добро пожаловать в наш бренный мир!");
+                }
+
+                dateOfBirth = value;
+            }
+        }
 
         public int Age
         {
@@ -47,9 +76,23 @@ namespace _61_62_USERS_AND_AWARDS.Entities
             Users.Add(this);
         }
 
+        private DateTime GetDateFromConsole(string userDate)
+        {
+            NullCheck(userDate);
+
+            bool isDate = DateTime.TryParseExact(userDate, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime userBirthDate);
+
+            if (!isDate)
+            {
+                throw new ArgumentException($"Date of birth must be in format: {DateFormat}:");
+            }
+
+            return userBirthDate;
+        }
+
         public void PrintObjects()
         {
-            foreach(var user in Users)
+            foreach (var user in Users)
             {
                 Console.WriteLine(user);
             }
@@ -57,12 +100,33 @@ namespace _61_62_USERS_AND_AWARDS.Entities
 
         public void Create()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void Delete()
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        private static void CheckName(string userName, string nameType)
+        {
+            NullCheck(userName);
+            NullCheck(nameType);
+
+            var userCharArray = userName.ToCharArray();
+
+            if (char.IsLower(userCharArray[0]))
+            {
+                throw new ArgumentException($"Поле '{nameType}' должно начинаться с заглавной буквы!");
+            }
+
+            foreach (char element in userCharArray)
+            {
+                if (!char.IsLetter(element))
+                {
+                    throw new ArgumentException($"Недопустимый символ в поле '{nameType}'!");
+                }
+            }
         }
 
         private static void NullCheck(string userString)
