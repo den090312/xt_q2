9823d2e3ea0a9adb84de3fb3d698afb9678d61fe
den090312 <31979965+den090312@users.noreply.github.com>
2019-08-16 20:11:41 +0400

    User, Award null checks

--- Task06/61-62_USERS AND AWARDS.BLL/AwardManager.cs
+++ Task06/61-62_USERS AND AWARDS.BLL/AwardManager.cs
@@ -4,9 +4,17 @@ namespace _61_62_USERS_AND_AWARDS.BLL
 {
     public static class AwardManager 
     {
-        public static void CreateAward(string name) => StorageManager.AddAward(new Award(name));
+        public static void CreateAward(string name)
+        {
+            StorageManager.NullCheck(name);
+            StorageManager.AddAward(new Award(name));
+        }
 
-        public static void DeleteAward(string name) => StorageManager.RemoveAward(name);
+        public static void DeleteAward(string name)
+        {
+            StorageManager.NullCheck(name);
+            StorageManager.RemoveAward(name);
+        }
 
         public static void PrintAllAwards() => StorageManager.PrintAllAwards();
     }
--- Task06/61-62_USERS AND AWARDS.BLL/UserManager.cs
+++ Task06/61-62_USERS AND AWARDS.BLL/UserManager.cs
@@ -5,9 +5,17 @@ namespace _61_62_USERS_AND_AWARDS.BLL
 {
     public static class UserManager
     {
-        public static void CreateUser(string name, DateTime birthDate) => StorageManager.AddUser(new User(name, birthDate));
+        public static void CreateUser(string name, DateTime birthDate)
+        {
+            StorageManager.NullCheck(name);
+            StorageManager.AddUser(new User(name, birthDate));
+        }
 
-        public static void DeleteUser(string name) => StorageManager.RemoveUser(name);
+        public static void DeleteUser(string name)
+        {
+            StorageManager.NullCheck(name);
+            StorageManager.RemoveUser(name);
+        }
 
         public static void PrintAllUsers() => StorageManager.PrintAllUsers();
     }
