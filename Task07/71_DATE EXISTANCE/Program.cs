72a2fff5f7851819d72cc2afe56a3fcf127f757d
den090312 <31979965+den090312@users.noreply.github.com>
2019-08-25 16:10:27 +0400

    logic

--- Task07/71_DATE EXISTANCE/Program.cs
+++ Task07/71_DATE EXISTANCE/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace _71_DATE_EXISTANCE
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly string dateFormat = "dd-mm-yyyy";
+
+        private static void Main(string[] args)
         {
+            Console.WriteLine($"Enter text with date in format: {dateFormat}");
+
+            var userText = Console.ReadLine();
+
+            if (userText.HasDate(dateFormat))
+            {
+                Console.WriteLine($"There is date in text: {userText}");
+            }
+            else
+            {
+                Console.WriteLine($"There is NO date in text: {userText}");
+            }
+        }
+    }
+
+    public static class DateExtensions
+    {
+        public static bool HasDate(this string userString, string dateFormat)
+        {
+            bool hasDate = false;
+
+            return hasDate;
         }
     }
 }
