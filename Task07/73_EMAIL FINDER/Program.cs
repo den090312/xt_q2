be77fe448db3e52337033546badfadca6abba565
Денис Болотин <den090312@gmail.com>
2019-08-26 10:25:58 +0400

    73

--- Task07/73_EMAIL FINDER/Program.cs
+++ Task07/73_EMAIL FINDER/Program.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _73_EMAIL_FINDER
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("Enter text");
+
+            var emailMatches = new Regex(@"[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*")
+                .Matches(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Emails found:");
+            Console.WriteLine("------------");
+
+            foreach (var match in emailMatches)
+            {
+                Console.WriteLine(match);
+            }
+
+            Console.WriteLine("------------");
+            Console.WriteLine();
         }
     }
 }
