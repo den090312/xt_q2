5249010617d8e8b4d290fdba4097b8c1e0df186b
den090312 <31979965+den090312@users.noreply.github.com>
2019-08-28 22:15:40 +0400

    73 merge fix

--- Task07/73_EMAIL FINDER/Program.cs
+++ Task07/73_EMAIL FINDER/Program.cs
@@ -10,52 +10,10 @@ namespace _73_EMAIL_FINDER
         {
             Console.WriteLine("Enter text");
 
-<<<<<<< HEAD
-            var emailMatchCollection = new Regex(@"[a-z\d]+[._-]*[a-z\d]+@[a-z\d-]+(\.([a-z-])+)+").Matches(Console.ReadLine());
-
-            var emailStringList = new List<string>();
-
-            foreach (var emailMatch in emailMatchCollection)
-            {
-                var emailMatchString = emailMatch.ToString();
-
-                if (emailMatchString.HasCorrectTopLvlDomain())
-                {
-                    emailStringList.Add(emailMatchString);
-                }
-            }
-
-            WriteEmails(emailStringList);
-        }
-
-        private static void WriteEmails(List<string> emailStringList)
-=======
             WriteEmailMatchList
             (
                 GetEmailMatchList(new Regex(@"[a-z\d]+[._-]*[a-z\d]+@[a-z\d]+(\.([a-z\d])+)+").Matches(Console.ReadLine()))
             );
-        }
-
-        private static void WriteEmailMatchList(List<string> emailMatchList)
->>>>>>> e03238c3157ff6d7fbf1fc39a3bdeda470167dee
-        {
-            Console.WriteLine();
-            Console.WriteLine("Emails found:");
-            Console.WriteLine("------------");
-
-<<<<<<< HEAD
-            foreach (var emailMatchString in emailStringList)
-            {
-                Console.WriteLine(emailMatchString);
-=======
-            foreach (var emailMatch in emailMatchList)
-            {
-                Console.WriteLine(emailMatch);
->>>>>>> e03238c3157ff6d7fbf1fc39a3bdeda470167dee
-            }
-
-            Console.WriteLine("------------");
-            Console.WriteLine();
         }
 
         private static List<string> GetEmailMatchList(MatchCollection emailMatchCollection)
@@ -74,22 +32,29 @@ namespace _73_EMAIL_FINDER
 
             return emailMatchList;
         }
+
+        private static void WriteEmailMatchList(List<string> emailMatchList)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Emails found:");
+            Console.WriteLine("------------");
+
+            foreach (var emailMatch in emailMatchList)
+            {
+                Console.WriteLine(emailMatch);
+            }
+
+            Console.WriteLine("------------");
+            Console.WriteLine();
+        }
     }
 
     public static class StringExtensions
     {
-<<<<<<< HEAD
-        public static bool HasCorrectTopLvlDomain(this string emailMatchString)
-        {
-            var lastIndexOf = emailMatchString.LastIndexOf('.');
-
-            var topLvlDomain = emailMatchString.Substring(lastIndexOf + 1, emailMatchString.Length - lastIndexOf - 1);
-=======
         public static bool HasCorrectTopLvlDomain(this string emailString)
         {
             var lastIndexOf = emailString.LastIndexOf('.');
             var topLvlDomain = emailString.Substring(lastIndexOf + 1, emailString.Length - lastIndexOf - 1);
->>>>>>> e03238c3157ff6d7fbf1fc39a3bdeda470167dee
 
             return Regex.IsMatch(topLvlDomain, "^[a-z]{2,6}$");
         }
