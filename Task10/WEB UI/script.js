c2127f15c27670ba8ac5d80994782d347c23e4e7
den090312 <31979965+den090312@users.noreply.github.com>
2019-09-15 01:06:27 +0400

    messages

--- Task10/WEB UI/script.js
+++ Task10/WEB UI/script.js
@@ -1,4 +1,12 @@
 ﻿window.onload = function () {
+    var message_done = document.getElementById("message_done");
+    var message_failure = document.getElementById("message_failure");
+
+    function disableMessages() {
+        message_done.style.display = "none";
+        message_failure.style.display = "none";
+    }
+
     var options = document.getElementsByClassName("options"); 
     var operations = document.getElementsByClassName("operations");
     if (options) {
@@ -7,79 +15,90 @@
     if (operations) {
         proceedToOperations(operations);
     }
-}
 
-function proceedToOptions(options, operations) {
-    for (let option of options) {
-        var buttons = option.getElementsByClassName("button");
-        if (buttons) {
-            for (let button of buttons) {
-                if (button) {
-                    buttonOnclick(button, operations);
+    function proceedToOptions(options, operations) {
+        for (let option of options) {
+            var buttons = option.getElementsByClassName("button");
+            if (buttons) {
+                for (let button of buttons) {
+                    if (button) {
+                        buttonOnclick(button, operations);
+                    }
                 }
             }
         }
     }
-}
 
-function buttonOnclick(button, operations) {
-    button.onclick = function (event) {
-        var main = document.getElementById("main");
-        var target = event.target;
-        if (target) {
-            switch (target.className) {
-                case "user_create":
-                    main.style.display = "none";
-                    operations[0].style.display = "block";
-                    var user_creation = document.getElementsByClassName("user_creation")[0];
-                    user_creation.style.display = "block";
-                    break;
-                case "user_delete":
-                    alert("user_delete");
-                    break;
-                case "user_print":
-                    alert("user_print");
-                    break;
-                case "award_create":
-                    alert("award_create");
-                    break;
-                case "award_delete":
-                    alert("award_delete");
-                    break;
-                case "award_print":
-                    alert("award_print");
-                    break;
-                case "join":
-                    alert("join");
-                    break;
-                case "exit":
-                    alert("exit");
-                    break;
-            }
-        }
-    };
-}
 
-function proceedToOperations(operations) {
-    for (let operation of operations) {
-        if (operation) {
-            operationOnClick(operation);    
-        }
-    }
-}
-
-function operationOnClick(operation) {
-    operation.onclick = function (event) {
-        var main = document.getElementById("main");  
-        var target = event.target;
-        if (target) {
-            switch (target.className) {
-                case "cancel":
-                    operation.style.display = "none";
-                    main.style.display = "block";
-                    break;
+    function proceedToOperations(operations) {
+        for (let operation of operations) {
+            if (operation) {
+                operationOnClick(operation);
             }
         }
     }
-}
 
+    function operationOnClick(operation) {
+        operation.onclick = function (event) {
+            disableMessages();
+            var main = document.getElementById("main");
+            var target = event.target;
+            if (target) {
+                switch (target.className) {
+                    case "cancel":
+                        operation.style.display = "none";
+                        main.style.display = "block";
+                        break;
+                    case "user_create":
+                        operation.style.display = "none";
+                        message_done.style.display = "block";
+                        main.style.display = "block";
+                        break;
+                }
+            }
+        }
+    }
+
+    function buttonOnclick(button, operations) {
+        button.onclick = function (event) {
+            disableMessages();
+            var main = document.getElementById("main");
+            var target = event.target;
+            if (target) {
+                switch (target.className) {
+                    case "user_create":
+                        userCreation(main);
+                        break;
+                    case "user_delete":
+                        alert("user_delete");
+                        break;
+                    case "user_print":
+                        alert("user_print");
+                        break;
+                    case "award_create":
+                        alert("award_create");
+                        break;
+                    case "award_delete":
+                        alert("award_delete");
+                        break;
+                    case "award_print":
+                        alert("award_print");
+                        break;
+                    case "join":
+                        alert("join");
+                        break;
+                    case "exit":
+                        alert("exit");
+                        break;
+                }
+            }
+        };
+
+        function userCreation(main) {
+            main.style.display = "none";
+            operations[0].style.display = "block";
+            var user_creation = document.getElementsByClassName("user_creation")[0];
+            user_creation.style.display = "block";
+        }
+    }
+}
