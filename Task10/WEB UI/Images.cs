cd0d6a1ea7f99064767d7bd48c6674d887a5d37f
den090312 <31979965+den090312@users.noreply.github.com>
2019-09-17 23:18:00 +0400

    Images refactor

--- Task10/WEB UI/Images.cs
+++ Task10/WEB UI/Images.cs
@@ -1,10 +1,37 @@
-﻿using Common;
-using System;
+﻿using System.IO;
+using System.Web;
 
 namespace WEB_UI
 {
     public static class Images
     {
+        public static bool ImageExists(HttpFileCollection images, out HttpPostedFile imageFile, out string imageFileName)
+        {
+            imageFile = images[0];
 
+            if (imageFile == null)
+            {
+                imageFileName = string.Empty;
+
+                return false;
+            }
+
+            if (images == null || images.Count == 0)
+            {
+                imageFileName = string.Empty;
+                imageFile = null;
+
+                return false;
+            }
+
+            imageFileName = Path.GetFileName(imageFile.FileName);
+
+            if (imageFileName == string.Empty)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
