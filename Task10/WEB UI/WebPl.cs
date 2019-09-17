bc0eb68d5ceb3db7efc342c1feed4bd99c83d956
den090312 <31979965+den090312@users.noreply.github.com>
2019-09-17 22:02:49 +0400

    null check

--- Task10/WEB UI/WebPl.cs
+++ Task10/WEB UI/WebPl.cs
@@ -7,6 +7,8 @@ namespace WEB_UI
     {
         public static bool UserCreated(string userName, string dateOfBirth)
         {
+            NullCheck(userName);
+
             if (!DateTime.TryParse(dateOfBirth, out DateTime result))
             {
                 return false;
@@ -20,6 +22,8 @@ namespace WEB_UI
 
         public static bool UserAwardsDeleted(string userGuid)
         {
+            NullCheck(userGuid);
+
             if (!Guid.TryParse(userGuid, out Guid result))
             {
                 return false;
@@ -30,6 +34,8 @@ namespace WEB_UI
 
         public static bool AwardCreated(string awardTitle)
         {
+            NullCheck(awardTitle);
+
             var awardLogic = DependencyResolver.AwardLogic;
             var award = awardLogic?.CreateAward(awardTitle);
 
@@ -38,12 +44,35 @@ namespace WEB_UI
 
         public static bool AwardUsersDeleted(string awardGuid)
         {
+            NullCheck(awardGuid);
+
             if (!Guid.TryParse(awardGuid, out Guid result))
             {
                 return false;
             }
 
             return DependencyResolver.UserAwardLogic.AwardUsersRemoved(result);
+        }
+
+        public static bool JoinedAwardToUser(string userGuid, string awardGuid)
+        {
+            NullCheck(userGuid);
+            NullCheck(awardGuid);
+
+            if (!Guid.TryParse(userGuid, out Guid resultUserGuid) || !Guid.TryParse(awardGuid, out Guid resultAwardGuid))
+            {
+                return false;
+            }
+
+            return DependencyResolver.UserAwardLogic.JoinedAwardToUser(resultUserGuid, resultAwardGuid);
+        }
+
+        private static void NullCheck<T>(T classObject) where T : class
+        {
+            if (classObject == null)
+            {
+                throw new NullReferenceException($"{nameof(classObject)} is null!");
+            }
         }
     }
 }
