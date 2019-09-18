b307daf89b390213641243cc6736acf22fcd596d
den090312 <31979965+den090312@users.noreply.github.com>
2019-09-18 22:19:27 +0400

    roles view model

--- Task10/WEB UI/WebUser.cs
+++ Task10/WEB UI/WebUser.cs
@@ -19,7 +19,7 @@ namespace WEB_UI
 
         public static WebUser Guest => new WebUser("Guest", Role.Guest, "Guest");
 
-        public static WebUser DefaultAdmin => new WebUser("admin", Role.Admin, "admin");
+        public static WebUser Admin => new WebUser("admin", Role.Admin, "admin");
 
         public enum Role
         {
