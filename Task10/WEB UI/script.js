9e2c9e72697655158f307931e7b36c1dea282969
den090312 <31979965+den090312@users.noreply.github.com>
2019-09-18 23:52:47 +0400

    log out view test

--- Task10/WEB UI/script.js
+++ Task10/WEB UI/script.js
@@ -25,6 +25,17 @@
         }
     }
 
+    var user_log_out = document.getElementsByClassName("user_log_out")[0];
+    if (user_log_out) {
+        log_out = user_log_out.getElementsByClassName("log_out")[0];
+        if (log_out) {
+            log_out_post = user_log_out.getElementsByClassName("log_out_post")[0];
+            if (log_out_post) {
+                log_out_post.value = "loggedOut";
+            }
+        }
+    }
+
     function setSelectedUserGuid() {
         var user_chosen = document.getElementsByClassName("user_chosen")[0];
         if (user_chosen) {
