fdabefbb3f5e990fb772a570a14a4189478d0d99
Денис Болотин <den090312@gmail.com>
2019-09-23 11:13:04 +0400

    RunScript moved to main

--- Task06/PL/OutputPl.cs
+++ Task06/PL/OutputPl.cs
@@ -10,11 +10,6 @@ namespace Pl
     {
         public void Run()
         {
-            //var connectionString = @"Data Source=DEN090312\SQLEXPRESS;Initial Catalog=webusersdb;Integrated Security=True";
-            //var scriptPath = @"C:\Users\Bolotin\source\repos\NewRepo2\Task06\Scripts\webusersdb.sql";
-
-            //WEB_UI.Database.RunScript(connectionString, scriptPath);
-
             new InputPl().Run();
         }
 
--- Task06/PL/Program.cs
+++ Task06/PL/Program.cs
@@ -4,6 +4,11 @@
     {
         private static void Main(string[] args)
         {
+            //var connectionString = @"Data Source=DEN090312\SQLEXPRESS;Initial Catalog=webusersdb;Integrated Security=True";
+            //var scriptPath = @"C:\Users\Bolotin\source\repos\NewRepo2\Task06\Scripts\webusersdb.sql";
+
+            //WEB_UI.Database.RunScript(connectionString, scriptPath);
+
             new OutputPl().Run();
         }
     }
