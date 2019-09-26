356efd663f113800bbdcb7f1daf762cefdbb97cc
den090312 <31979965+den090312@users.noreply.github.com>
2019-09-26 21:18:37 +0400

    DependencyResolver app.config

--- Task06/Common/DependencyResolver.cs
+++ Task06/Common/DependencyResolver.cs
@@ -2,34 +2,66 @@
 using InterfacesBLL;
 using InterfacesDAL;
 using DAL;
+using System.Configuration;
 
 namespace Common
 {
     public static class DependencyResolver
     {
-        private static readonly IUserLogic _userLogic;
-        private static readonly IUserDao userDao;
-        private static readonly IAwardLogic _awardLogic;
-        private static readonly IAwardDao awardDao;
-        private static readonly IUserAwardLogic _userAwardLogic;
         private static readonly IUserAwardDao userAwardDao;
+        private static readonly IUserDao userDao;
+        private static readonly IAwardDao awardDao;
+
+        private static readonly IUserLogic _userLogic;
+        private static readonly IAwardLogic _awardLogic;
+        private static readonly IUserAwardLogic _userAwardLogic;
 
         public static IUserLogic UserLogic => _userLogic;
 
         public static IAwardLogic AwardLogic => _awardLogic;
 
-        public static IUserAwardLogic UserAwardLogic => _userAwardLogic;
+        public static IUserAwardLogic UserAwardLogic => _userAwardLogic; 
 
         static DependencyResolver()
         {
-            //userDao = new UserDaoFile();
-            userDao = new UserDaoDb();
+            var userAwardDaoSet = ConfigurationManager.AppSettings["userAwardDao"];
+
+            if (userAwardDaoSet == "1")
+            {
+                userAwardDao = new UserAwardDaoFile();
+            }
+
+            if (userAwardDaoSet == "2")
+            {
+                userAwardDao = new UserAwardDaoDb();
+            }
+
+            var userDaoSet = ConfigurationManager.AppSettings["userDao"];
+
+            if (userDaoSet == "1")
+            {
+                userDao = new UserDaoFile();
+            }
+
+            if (userDaoSet == "2")
+            {
+                userDao = new UserDaoDb();
+            }
+
+            var awardDaoSet = ConfigurationManager.AppSettings["awardDao"];
+
+            if (awardDaoSet == "1")
+            {
+                awardDao = new AwardDaoFile();
+            }
+
+            if (awardDaoSet == "2")
+            {
+                awardDao = new AwardDaoDb();
+            }
+
             _userLogic = new UserLogic(userDao);
-            //awardDao = new AwardDaoFile();
-            awardDao = new AwardDaoDb();
             _awardLogic = new AwardLogic(awardDao);
-            //userAwardDao = new UserAwardDaoFile();
-            userAwardDao = new UserAwardDaoDb();
             _userAwardLogic = new UserAwardLogic(userAwardDao, userDao, awardDao);
         }
     }
